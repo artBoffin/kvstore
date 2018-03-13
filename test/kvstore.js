a595dea76d63995c6e420a3178f7ea5037b2e2e5
Will Hay Jr <willhayjr@gmail.com>
2018-03-13 01:36:12 -0400

    test files

---
+++ test/kvstore.js
@@ -0,0 +1,30 @@
+var KVStore = artifacts.require("./KVStore.sol");
+
+
+contract('KVStore', function (accounts) {
+  it("should store a key value pair in the mapping", function () {
+    return KVStore.deployed().then(inst => { kvstore = inst }).then(function () {
+      return kvstore.set("satoshi", "hello world").then(function () {
+        return kvstore.get(accounts[0], "satoshi").then(function (value) {
+          return assert.equal(value, "hello world", "value was not saved in mapping");
+        });
+      });
+    });
+  });
+  it("should restrict key/value setting to address that owns mapping", function () {
+    return KVStore.deployed().then(inst => { kvstore = inst }).then(function () {
+      return kvstore.set("satoshi", "hello world", { from: accounts[0] }).then(function () {
+        return kvstore.set("satoshi", "gox", { from: accounts[1] }).then(function () {
+        }).then(function () {
+          return kvstore.get(accounts[0], "satoshi").then(function (value) {
+            var val0 = value;
+            return kvstore.get(accounts[1], "satoshi").then(function (value) {
+              return assert.notEqual(val0, value, "key value pairs saved by different address are equal");
+            })
+          });
+        })
+      });
+    });
+  });
+
+});
