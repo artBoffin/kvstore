dc59c2d3b5002bec01e46c3e3b5c90b0fc7b54f7
Will Hay Jr <willhayjr@gmail.com>
2018-03-24 16:10:51 -0400

    push to rinkeby testnet

--- truffle.js
+++ truffle.js
@@ -1,3 +1,14 @@
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const MNEMONIC = process.env.MNEMONIC;
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const NETWORK_IDS = {
+  // mainnet: 1,
+  ropsten: 2,
+  rinkeby: 4,
+  kovan: 42
+};
+
 module.exports = {
   networks: {
     development: {
@@ -7,3 +18,10 @@ module.exports = {
     }
   }
 };
+
+for (let networkName in NETWORK_IDS) {
+  module.exports.networks[ networkName ] = {
+    provider: new HDWalletProvider(MNEMONIC, 'https://' + networkName + '.infura.io/' + INFURA_API_KEY),
+    network_id: NETWORK_IDS[ networkName ]
+  };
+}
